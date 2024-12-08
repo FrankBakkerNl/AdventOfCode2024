1c454bce8d7d6f95b17ff7686ac1e50ee5c7e095
Frank Bakker <bakker.frank@gmail.com>
2024-12-08 21:06:20 +0100

    Day 7-2

--- AdventOfCode2024/Puzzles/D07/Day07.cs
+++ AdventOfCode2024/Puzzles/D07/Day07.cs
@@ -16,33 +16,34 @@ public class Day07
     static bool FindPattern(Equation equation)
     {
         var maxPattern = 1 << equation.Terms.Length - 1;
-        
-        for (int pattern = 0; pattern < maxPattern; pattern++)
+
+        for (var pattern = 0; pattern < maxPattern; pattern++)
         {
-            if (HasValue(equation, pattern))
-                return true;
+            if (HasValue(equation, pattern)) return true;
         }
 
         return false;
     }
 
-    static bool HasValue(Equation equation, int pattern)
+    static bool HasValue(Equation equation, int operatorPattern)
     {
-        long register = equation.Terms[0];
-        int mask = 1;
-        for (int i = 1; i < equation.Terms.Length; i++)
+        var terms = equation.Terms;
+        long register = terms[0];
+        var mask = 1;
+
+        for (var i = 1; i < terms.Length; i++)
         {
-            if ((pattern & mask) == 0) // means add
+            if ((operatorPattern & mask) == 0) // means add
             {
-                register += equation.Terms[i];
+                register += terms[i];
             }
             else
             {
-                register *= equation.Terms[i];
+                register *= terms[i];
             }
 
             if (register > equation.Result) return false;
-            
+
             // move the mask
             mask <<= 1;
         }
@@ -50,25 +51,41 @@ public class Day07
         return register == equation.Result;
     }
 
-    //[Result(0)]
-    [TestCase(result: 0)]
+    [Result(271691107779347)]
+    [TestCase(result: 11387)]
     public static long GetAnswer2(string[] input)
     {
-        return 0;
+        var equations = ParseInput(input);
+        
+        return equations
+            .Where(equation => HasPossibleValueRecursive(equation, 0, 0))
+            .Sum(e => e.Result);
     }
 
-    static Equation[] ParseInput(string[] input)
+    static bool HasPossibleValueRecursive(Equation equation, long register, int termsUsed)
     {
-        return input.Select(ParseLine).ToArray();
+        // Stop recursion
+        if (termsUsed == equation.Terms.Length) return register == equation.Result;
+    
+        if (register > equation.Result) return false; // values only increase so we can stop now
+
+        var nextTerm = equation.Terms[termsUsed];
+
+        return HasPossibleValueRecursive(equation, termsUsed == 0 ? nextTerm : register * nextTerm, termsUsed + 1) ||
+               HasPossibleValueRecursive(equation, register + nextTerm, termsUsed + 1) ||
+               HasPossibleValueRecursive(equation, Concat(register, nextTerm), termsUsed + 1);
     }
+
+    static long Concat(long x, long y) => long.Parse(x.ToString() + y.ToString());
+
+    static Equation[] ParseInput(string[] input) => input.Select(ParseLine).ToArray();
 
     private static Equation ParseLine(string line)
     {
         var split = line.IndexOf(":", StringComparison.Ordinal);
         return new Equation(long.Parse(line[..split]),
-            line[(split+2) ..].Split(' ', StringSplitOptions.TrimEntries).Select(int.Parse).ToArray());
+            line[(split + 2) ..].Split(' ', StringSplitOptions.TrimEntries).Select(int.Parse).ToArray());
     }
-
 
     record Equation(long Result, int[] Terms);
 }
